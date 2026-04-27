06ac761604f27844dfaedd157c20416074e718a2
TechNABA <tech@naba.it>
2026-04-27 10:33:02 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ export default defineConfig({
   // For a project page (https://<user>.github.io/<repo>/) set `base: '/<repo>/'`.
   // For a custom domain (rd.naba.it) set `site: 'https://rd.naba.it'` and remove `base`.
   site: 'https://technaba.github.io',
-  base: '/virtual-studio-naba',
+  base: '/virtual-studio-website',
   integrations: [tailwind()],
 });
